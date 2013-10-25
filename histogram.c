d8dbe7862e5d2f4477c9c08af3951b092f9709de
Bryan Ching <bryan792@gmail.com>
2013-10-25 11:57:57 -0700

    Format

--- histogram.c
+++ histogram.c
@@ -12,7 +12,7 @@ void newMap(int itask, char *str, int size, void *kv, void *ptr);
 void binMap(int, void *, void *);
 void sum(char *, int, char *, int, int *, void *, void *);
 int ncompare(char *, int, char *, int);
-void m_prepareoutput(uint64_t, char *, int, char *,int, void *, void *);
+void m_prepareoutput(uint64_t, char *, int, char *, int, void *, void *);
 void output(uint64_t, char *, int, char *, int , void *, void *);
 
 typedef struct
@@ -22,8 +22,9 @@ typedef struct
 
 /* ---------------------------------------------------------------------- */
 
-int myhash(char * key, int keysize){
-    return strtol(key, NULL, 10) %4;   
+int myhash(char * key, int keysize)
+{
+  return strtol(key, NULL, 10) % 4;
 }
 
 int main(int narg, char **args)
@@ -32,7 +33,6 @@ int main(int narg, char **args)
   int nwords, nunique;
   double tstart, tstop;
   Count count;
-
   MPI_Init(&narg, &args);
   MPI_Comm_rank(MPI_COMM_WORLD, &me);
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
@@ -43,75 +43,60 @@ int main(int narg, char **args)
     {
       printf("Syntax: cwordfreq file1 file2 ...\n");
     }
+
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
 
   void *mr = (void *) MR_create(MPI_COMM_WORLD);
   //MR_set_verbosity(mr, 2);
-
   MPI_Barrier(MPI_COMM_WORLD);
   int test = 1;
-
   //nwords = MR_map(mr, narg - 1, &binMap, &args[1]);
   nwords = MR_map_file_char(mr, nprocs, 1, &args[1], 0, 0, ' ', 100, &newMap, NULL);
-
   MPI_Barrier(MPI_COMM_WORLD);
-  MR_collate(mr,NULL);
-
+  MR_collate(mr, NULL);
   MPI_Barrier(MPI_COMM_WORLD);
   nunique = MR_reduce(mr, &sum, NULL);
-
   MPI_Barrier(MPI_COMM_WORLD);
 //  MR_gather(mr, 1);
-
   MR_map_mr(mr, mr, m_prepareoutput, NULL);
-
-MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Barrier(MPI_COMM_WORLD);
   MR_gather(mr, 1);
-
   MPI_Barrier(MPI_COMM_WORLD);
 //  MR_collate(mr, NULL);
-
 //  MPI_Barrier(MPI_COMM_WORLD);
 //  nunique = MR_reduce(mr, &sum, NULL);
-  
-printf("%d words\n%d unique\n", nwords, nunique);
-
+  printf("%d words\n%d unique\n", nwords, nunique);
   //MR_map_mr(mr, mr, &output, NULL);
   //MPI_Barrier(MPI_COMM_WORLD);
   //MR_sort_values(mr,&ncompare);
-
-  MR_map_mr(mr, mr ,&output,NULL);
-
+  MR_map_mr(mr, mr , &output, NULL);
   //MR_gather(mr,1);
   //MR_sort_values(mr,&ncompare);
-
-
   MR_destroy(mr);
   MPI_Finalize();
 }
 
-void newMap(int itask, char *str, int size, void *kv, void *ptr){
-    char *whitespace = " \t\n\f\r\0";
-   char *word = strtok(str, whitespace);
-   float f;
-   int i;
-   char key[10];
+void newMap(int itask, char *str, int size, void *kv, void *ptr)
+{
+  char *whitespace = " \t\n\f\r\0";
+  char *word = strtok(str, whitespace);
+  float f;
+  int i;
+  char key[10];
 
-   while (word)
-   {
-     f = strtof(word, NULL);
+  while (word)
+  {
+    f = strtof(word, NULL);
 //     f = (int)((f + 10) / .5);
 //     i = (int) f;
-
-     i = (int) ((f + 10) / .5);
+    i = (int) ((f + 10) / .5);
 //     sprintf(key, "%i", i);
 //     MR_kv_add(kv, key, 10, NULL, 0);
-     MR_kv_add(kv, &i, sizeof(int), NULL, 0);
-     //printf("%d\n",i );
-     word = strtok(NULL, whitespace);
-   }
-
+    MR_kv_add(kv, &i, sizeof(int), NULL, 0);
+    //printf("%d\n",i );
+    word = strtok(NULL, whitespace);
+  }
 }
 
 void binMap(int itask, void *kv, void *ptr)
@@ -143,10 +128,8 @@ void binMap(int itask, void *kv, void *ptr)
     f = strtof(word, NULL);
     f = (int)((f + 10) / .5);
     i = (int) f;
-
     memset(key, 0, 10);
     sprintf(key, "%i", i);
-
     MR_kv_add(kv, key, sizeof(i), NULL, 0);
     //printf("%d\n",i );
     word = strtok(NULL, whitespace);
@@ -161,7 +144,7 @@ void sum(char *key, int keybytes, char *multivalue,
          int nvalues, int *valuebytes, void *kv, void *ptr)
 {
   MR_kv_add(kv, key, keybytes, (char *) &nvalues, sizeof(int));
-  printf("%s %d\n",key,nvalues);
+  printf("%s %d\n", key, nvalues);
 }
 
 /* ----------------------------------------------------------------------
@@ -192,13 +175,12 @@ void m_prepareoutput(uint64_t itask, char *key, int keybytes, char *value,
                      int valuebytes, void *kv, void *ptr)
 {
   int n = *(int *) value;
- 
-  MR_kv_add(kv, key, keybytes,(char *) &n,sizeof(int));
+  MR_kv_add(kv, key, keybytes, (char *) &n, sizeof(int));
 }
 
 void output(uint64_t itask, char *key, int keybytes, char *value,
             int valuebytes, void *kv, void *ptr)
 {
   int n = *(int *) value;
-  printf("key:%i  value:%i\n",*(int *) key, n);
+  printf("key:%i  value:%i\n", *(int *) key, n);
 }
